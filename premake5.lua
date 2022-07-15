ed8cf8ea929d9199b39d1d96f4810a5626e4dc86
lore14th <lorenzo.fortina.95@gmail.com>
2022-07-15 15:34:57 +0200

    Update premake. Added Distribution configuration

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "spdlog"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
-	staticruntime "On"
+	staticruntime "Off"
 	
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -31,10 +31,14 @@ project "spdlog"
 
 	filter "configurations:Debug"
 		runtime "Debug"
-		symbols "On"	-- debug version --
+		symbols "On"
 
 	filter "configurations:Release"
 		runtime "Release"
-		optimize "Full"	-- release version --
-		inlining "Auto"
-		floatingpoint "Fast"
+		optimize "On"
+		symbols "On"
+
+	filter "configurations:Distribution"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
